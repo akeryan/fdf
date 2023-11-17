a7905263c42cf715e9c1a52d1402077d50f7fff8
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-11-17 16:31:38 +0400

    GET_MAP_DIMENSIONS() added

--- fdf.h
+++ fdf.h
@@ -6,32 +6,40 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 09:31:35 by akeryan           #+#    #+#             */
-/*   Updated: 2023/11/16 14:00:25 by akeryan          ###   ########.fr       */
+/*   Updated: 2023/11/17 16:09:38 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include "./minilibx/mlx.h" 
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "./minilibx/mlx.h" 
+# include "../libft/libft.h"
 
 # define WINDOW_WIDTH 800 
 # define WINDOW_HEIGHT 600
 # define MLX_ERROR 1
 
-
-typedef struct s_img_data 
+typedef struct s_img_data
 {
-	int pixel_bits;
-	int line_bytes; 
-	int endian;
-}	t_img_data;
+	int	pixel_bits;
+	int	line_bytes;
+	int	endian;
+}	t_idata;
 
+typedef struct s_2darray_size
+{
+	int	rows;
+	int	columns;
+}	t_2dsize;
 
-void colorize_pixel(void *mlx, char *buffer, int pixel, int color, t_img_data i_d);
-void check_ptr(void *ptr);
-void mlx_end(void *mlx_ptr, void *win_ptr);
+void	colorize_pixel(char *buf, int pix, int color, t_idata d);
+void	check_ptr(void *ptr);
+t_2dsize	*get_map_dimensions(int fd);
+void	mlx_end(void *mlx_ptr, void *win_ptr);
+int	num_of_numbers(char *str);
 
-# endif 
+#endif
--- src/mlx_utils.c
+++ src/mlx_utils.c
@@ -6,43 +6,87 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 11:09:32 by akeryan           #+#    #+#             */
-/*   Updated: 2023/11/16 14:00:15 by akeryan          ###   ########.fr       */
+/*   Updated: 2023/11/17 16:30:05 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
-void check_ptr(void *ptr)
+void	check_ptr(void *ptr)
 {
-	if(ptr == NULL)
+	if (ptr == NULL)
 	{
 		printf("ptr is NULL\n");
-        exit(MLX_ERROR);
+		exit(MLX_ERROR);
 	}
 }
-void mlx_end(void *mlx_ptr, void *win_ptr)
+
+void	mlx_end(void *mlx_ptr, void *win_ptr)
 {
-    mlx_destroy_window(mlx_ptr, win_ptr);
-    free(mlx_ptr);
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	free(mlx_ptr);
 }
 
-void colorize_pixel(void *mlx, char *buffer, int pixel, int color, t_img_data i_d)
+int **data_casting(int fd)
 {
-	if (i_d.pixel_bits != 32)
-		color = mlx_get_color_value(mlx, color);
+	char	*line;
+	int		rows;
+	int		cols;
 
-	if (i_d.endian == 1)        // Most significant (Alpha) byte first
+}
+
+t_2dsize	*get_map_dimensions(int fd)
+{
+	t_2dsize	*size;
+	char		*line;
+	int			rows;
+
+	size = (t_2dsize *)malloc(sizeof(t_2dsize));
+	line = get_next_line(fd);
+	size->columns = num_of_numbers(line);
+	size->rows = 0;
+	while (42)
 	{
-		buffer[pixel + 0] = (color >> 24);
-		buffer[pixel + 1] = (color >> 16) & 0xFF;
-		buffer[pixel + 2] = (color >> 8) & 0xFF;
-		buffer[pixel + 3] = (color) & 0xFF;
+		line = get_next_line(fd);
+		if (line)
+			size->rows++;
+		else
+			break ;
 	}
-	else if (i_d.endian == 0)   // Least significant (Blue) byte first
+	return (size);
+}
+
+int	num_of_numbers(char *str)
+{
+	int	num;
+
+	num = 0;
+	while (*str != '\0' && *str != '\n')
 	{
-		buffer[pixel + 0] = (color) & 0xFF;
-		buffer[pixel + 1] = (color >> 8) & 0xFF;
-		buffer[pixel + 2] = (color >> 16) & 0xFF;
-		buffer[pixel + 3] = (color >> 24);
+		while (*str == 32 || (*str >= 9 && *str <= 13))
+			str++;
+		if (ft_isdigit(*str++))
+			num++;
+		while (ft_isdigit(*str++))
+			;
+	}
+	return (num);
+}
+
+void	colorize_pixel(char *buf, int pix, int color, t_idata i_d)
+{
+	if (i_d.endian == 1)
+	{
+		buf[pix + 0] = (color >> 24);
+		buf[pix + 1] = (color >> 16) & 0xFF;
+		buf[pix + 2] = (color >> 8) & 0xFF;
+		buf[pix + 3] = (color) & 0xFF;
+	}
+	else if (i_d.endian == 0)
+	{
+		buf[pix + 0] = (color) & 0xFF;
+		buf[pix + 1] = (color >> 8) & 0xFF;
+		buf[pix + 2] = (color >> 16) & 0xFF;
+		buf[pix + 3] = (color >> 24);
 	}
 }
