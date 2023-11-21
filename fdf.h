98b48a0ca4488ac75e347ceacb4804a223851f6d
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-11-21 20:44:04 +0400

    window is closing with ESC

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 09:31:35 by akeryan           #+#    #+#             */
-/*   Updated: 2023/11/21 13:52:56 by akeryan          ###   ########.fr       */
+/*   Updated: 2023/11/21 19:50:59 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,12 @@
 # define WINDOW_HEIGHT 600
 # define MLX_ERROR 1
 
+typedef struct s_data
+{
+	void	*mlx;
+	void	*win;
+}	t_data;
+
 typedef struct s_img_data
 {
 	int	pixel_bits;
@@ -37,12 +43,6 @@ typedef struct s_array
 	int	rows;
 	int	cols;
 }	t_array;
-
-typedef struct s_2darray_size
-{
-	int	rows;
-	int	columns;
-}	t_2dsize;
 
 typedef struct s_node
 {
@@ -74,5 +74,8 @@ t_lst		*new_map(void);
 t_array		*new_tarray(int rows, int cols);
 void		print_arr(t_array *arr);
 t_array		*array_from_map(t_lst *map);
+
+//hooks
+int			close_window(int key, t_data *d);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,21 +6,21 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 11:09:50 by akeryan           #+#    #+#             */
-/*   Updated: 2023/11/21 13:51:12 by akeryan          ###   ########.fr       */
+/*   Updated: 2023/11/21 20:14:50 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
+	t_data	sys;
 	t_idata	i_d;
 	unsigned int color_value;
 	int color = 0xAB48EF;
 
-	
-	//void *mlx = mlx_init();
-    //void *win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Tutorial Window - Create Image");
+	sys.mlx = mlx_init();
+    sys.win = mlx_new_window(sys.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Tutorial Window - Create Image");
 	//void *image = mlx_new_image(mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	//char *buffer = mlx_get_data_addr(image, &i_d.pixel_bits, &i_d.line_bytes, &i_d.endian);
 	//if (i_d.pixel_bits != 32)
@@ -37,19 +37,17 @@ int main(int argc, char *argv[])
 		//}
 	//}
 	//mlx_put_image_to_window(mlx, win, image, 0, 0);
-	t_lst	*map;
-	map = read_map(argv[1]);
+	//t_lst	*map;
+	//map = read_map(argv[1]);
 
-	print_map(map);
-	printf("%d\n", get_num_of_columns(map));
-	printf("num of nodes: %d\n", map->len);
+	//print_map(map);
+	//printf("%d\n", get_num_of_columns(map));
+	//printf("num of nodes: %d\n", map->len);
 
-	t_array *a = array_from_map(map); 
-	print_arr(a);
-	//int **arr = map_to_array(map);
-	//int rows = get_num_of_rows(map);
-	//int cols = get_num_of_columns(map);
-	
-
-    //mlx_loop(mlx);
+	//t_array *a = array_from_map(map); 
+	//print_arr(a);
+	//mlx_loop_hook(sys.mlx, close_window, &sys);
+    mlx_key_hook(sys.win, close_window, &sys);
+    mlx_loop(sys.mlx);
+	//mlx_destroy_window(sys->mlx, sys->win);
 }
