f68bca5244b792ef26d4b8c021204e445e277f47
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-11-23 09:36:18 +0400

    new_pnt3d() function added
    - t_pnt3d *new_pnt3d(int x, int y, int z)
    - allocates memory, assigns values and returns the pointer

--- src/struct.c
+++ src/struct.c
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 17:14:51 by akeryan           #+#    #+#             */
-/*   Updated: 2023/11/22 17:32:31 by akeryan          ###   ########.fr       */
+/*   Updated: 2023/11/23 09:36:06 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,20 @@ void	*new_pair(int x0, int y0, int x1, int y1)
 	p->y0 = y0;
 	p->y1 = y1;
 	return (p);
+}
+
+t_pnt3d	*new_pnt3d(int x, int y, int z)
+{
+	t_pnt3d	*a;
+
+	a = (t_pnt3d *)malloc(sizeof(t_pnt3d));
+	if (a == NULL)
+	{
+		perror("Memory allocation faild in new_pnt3d()");
+		return (NULL);
+	}
+	a->x = x;
+	a->y = y;
+	a->z = z;
+	return (a);
 }
