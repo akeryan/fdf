445ebbc5400762bb7ea29c282e7ecc555dd1ebff
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-11-27 16:29:29 +0400

    FILE OPENING IS PROTECTED
    protected from:
    - opening more than 1 file
    - opening nonexistent file
    - opening protected file
    - running app without mentioning file

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 11:09:50 by akeryan           #+#    #+#             */
-/*   Updated: 2023/11/27 15:46:18 by akeryan          ###   ########.fr       */
+/*   Updated: 2023/11/27 16:26:01 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,16 @@
 int	main(int argc, char *argv[])
 {
 	t_data	d;
+	int		fd;
 
+	if (argc > 2 || !argv[1])
+		exit (0);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Faild to open file");
+		exit (0);
+	}
 	initialize(&d, argv[1]);
 	put_obj_to_origin(d.obj);
 	transform(d.obj, &d);
